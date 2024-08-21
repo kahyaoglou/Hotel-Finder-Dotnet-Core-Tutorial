3e7be72834fd9b29c6599504c674993223a4b219
Furkan Eralp Kahyaoğlu <furkan.kahyaoglu@beymen.com>
2024-08-21 17:13:28 +0300

    Business katmanı eklendi.

---
+++ HotelFinder.Business/Abstract/IHotelService.cs
@@ -0,0 +1,23 @@
+﻿using HotelFinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.Business.Abstract
+{
+    public interface IHotelService
+    {
+        List<Hotel> GetAllHotels();
+        //Geriye liste türünden Hotel'leri döndürecek. Parametre almayacak.
+        Hotel GetHotelById(int id);
+        //Geriye Hotel döndürecek. Parametre id alacak.
+        Hotel CreateHotel(Hotel hotel);
+        //Geriye Hotel döndürecek. Parametre Hotel alacak.
+        Hotel UpdateHotel(Hotel hotel);
+        //Geriye Hotel döndürecek. Parametre Hotel alacak.
+        void DeleteHotel(int id);
+        //Geriye bir şey döndürmeyecek. Parametre id alacak.
+    }
+}
---
+++ HotelFinder.Business/Concrete/HotelManager.cs
@@ -0,0 +1,65 @@
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.DataAccess.Abstract;
+using HotelFinder.DataAccess.Concrete;
+using HotelFinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.Business.Concrete
+{
+    public class HotelManager : IHotelService
+    {
+        private IHotelRepository _hotelRepository;
+        //IHotelRepository interface türünden bir değişken oluşturduk.
+        public HotelManager()
+        {
+            _hotelRepository = new HotelRepository();
+            //Bu değişken, bir HotelRepository örneği olsun dedik.
+        }
+
+        public Hotel CreateHotel(Hotel hotel)
+        {
+            return _hotelRepository.CreateHotel(hotel);
+        }
+
+        public void DeleteHotel(int id)
+        {
+            _hotelRepository.DeleteHotel(id);
+        }
+
+        public List<Hotel> GetAllHotels()
+        {
+            return _hotelRepository.GetAllHotels();
+        }
+
+        public Hotel GetHotelById(int id)
+        {
+            //Bir iş kuralı yazalım.
+            if (id > 0)
+            {
+                return _hotelRepository.GetHotelById(id);
+            }
+
+            throw new Exception("id can not be less than 1");
+        }
+
+        public Hotel UpdateHotel(Hotel hotel)
+        {
+            return _hotelRepository.UpdateHotel(hotel);
+        }
+    }
+}
+
+/*
+ - Bizim burada işin mantığını yönetmemiz gerekecek. Entities katmanında veritabanına yansıyan verileri
+ property olarak tanımlamıştık.
+
+ - DataAccess katmanında veritabanı üzerindeki temel işlemleri yapmamızı sağlayan yapıyı kurmuştuk.
+
+ - Bu katmanda da basit veritabanı işlemleri için gereken iş kurallarını yapacağız. Bu katman genellikle isterlerin
+ tam anlamıyla karşılanması için gerekli koşulların yazıldığı katmandır.
+*/
+
